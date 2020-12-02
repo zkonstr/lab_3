2cc4592760be3bc043b6bd4fdd3ab2c9220459f0
Konstantin <zkonstr@gmail.com>
2020-12-02 20:10:34 +0300

    dev1
    gorner is ready

--- lab3/src/bsu/rfe/java/group6/lab3/IZhibul/varB7/GornerTableModel.java
+++ lab3/src/bsu/rfe/java/group6/lab3/IZhibul/varB7/GornerTableModel.java
@@ -9,8 +9,7 @@ public class GornerTableModel extends AbstractTableModel {
     private Double to;
     private Double step;
 
-    public GornerTableModel(Double from, Double to, Double step,
-                            Double[] coefficients) {
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients) {
         this.from = from;
         this.to = to;
         this.step = step;
@@ -41,19 +40,27 @@ public class GornerTableModel extends AbstractTableModel {
     }
 
     public Object getValueAt(int row, int col) {
-// Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
+        // Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
         double x = from + step * row;
+        double[] result = new double[3];
         if (col == 0) {
-// Если запрашивается значение 1-го столбца, то это X
+            // Если запрашивается значение 1-го столбца, то это X
             return x;
-        } else {
-// Если запрашивается значение 2-го столбца, то это значение
-// многочлена
-            Double result = 0.0;
-// Вычисление значения в точке по схеме Горнера.
-// Вспомнить 1-ый курс и реализовать
-            return result;
-        }
+        } else if (col == 1) {
+            result[0] = 0.0;
+            for (int i = 0; i < coefficients.length; i++) {
+                result[0] += Math.pow(x, coefficients.length - 1 - i) * coefficients[i];
+            }
+            return result[0];
+        } else if (col == 2) {
+            result[1] = 0.0;
+            int p = coefficients.length - 1;
+            for (int i = 0; i < coefficients.length; i++) {
+                result[1] += Math.pow(x, coefficients.length - 1 - i) * coefficients[p--];
+            }
+            return result[1];
+        } else
+            return result[2] = result[1] - result[0];
     }
 
     public String getColumnName(int col) {
